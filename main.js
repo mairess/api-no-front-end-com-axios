3cfaa6d92138acf682995ef9e2e84e19c8b8189f
Maires Souza <trabalho.maires@gmail.com>
2023-04-25 07:58:32 -0300

    ignore .vscode

---
+++ main.js
@@ -0,0 +1,12 @@
+const url = "http://localhost:5500/api" 
+
+function getUsers(){
+    axios.get(url)
+    .then(response => {
+        apiResult.textContent = JSON.stringify(response.data)
+    })
+    .catch(error => console.error(error))
+}
+
+getUsers()
+
