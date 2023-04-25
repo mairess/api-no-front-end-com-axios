b42df6a6da898509c3d60fb086d7b46f81b4d574
Maires Souza <trabalho.maires@gmail.com>
2023-04-25 08:10:51 -0300

    add POST

--- main.js
+++ main.js
@@ -8,5 +8,21 @@ function getUsers(){
     .catch(error => console.error(error))
 }
 
+function addNewUser(newUser){
+    axios.post(url, newUser)
+    .then(response => {
+        console.log(response)
+    })
+    .catch(error => console.error(error))
+}
+
 getUsers()
+
+const newUser = {
+    name: "Carlos Antônio",
+    avatar: "https://picsum.photos/200/300",
+    city: "Belem"
+}
+
+// addNewUser(newUser)
 
